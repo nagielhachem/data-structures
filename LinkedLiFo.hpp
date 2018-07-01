94618fcb439d01e68a4107ade30c3b5bfbe62c78
nagielhachem <nagielhachem@gmail.com>
2018-07-01 13:51:06 +0200

    Define Linked LiFo class

---
+++ LinkedLiFo.hpp
@@ -0,0 +1,28 @@
+//
+//  LinkedLiFo.hpp
+//  CollectionsTd
+//
+//  Created by Nagi's Macbook Air on 10/8/15.
+//  Copyright © 2015 Nagi's Macbook Air. All rights reserved.
+//
+
+#ifndef LinkedLiFo_hpp
+#define LinkedLiFo_hpp
+
+#include "LiFo.hpp"
+#include "LinkedList.hpp"
+
+class LinkedLiFo : public LiFo {
+    LinkedList values;
+    
+public:
+    LinkedLiFo();
+    void push(float value);
+    void pop();
+    float top();
+    bool isEmpty();
+    ~LinkedLiFo();
+};
+
+
+#endif /* LinkedLiFo_hpp */
