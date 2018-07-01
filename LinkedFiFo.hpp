982b3d21ae102d4fd69ea95c6f2472f2b12d9e07
nagielhachem <nagielhachem@gmail.com>
2018-07-01 13:50:23 +0200

    Define Linked FiFo class

---
+++ LinkedFiFo.hpp
@@ -0,0 +1,31 @@
+//
+//  LinkedFiFo.hpp
+//  CollectionsTd
+//
+//  Created by Nagi's Macbook Air on 10/8/15.
+//  Copyright © 2015 Nagi's Macbook Air. All rights reserved.
+//
+
+#ifndef LinkedFiFo_hpp
+#define LinkedFiFo_hpp
+
+#include "FiFo.hpp"
+
+class LinkedFiFo : public FiFo {
+    struct Noeud {
+        float v;
+        Noeud* n;
+    } * tete, *queue;
+    
+public:
+    LinkedFiFo();
+    LinkedFiFo(LinkedFiFo const&);
+    LinkedFiFo const& operator=(LinkedFiFo const&);
+    void push(float value);
+    void pop();
+    float top();
+    bool isEmpty();
+    ~LinkedFiFo();
+};
+
+#endif /* LinkedFiFo_hpp */
